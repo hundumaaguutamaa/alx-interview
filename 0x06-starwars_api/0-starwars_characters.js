75bb7296780d2109af725ab688002e2de3e291f2
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-19 05:24:08 +0300

    fetch character updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,33 @@
 
 const request = require('request');
 
-const movieIdentifier = process.argv[2];
-const movieURL = 'https://swapi-api.alx-tools.com/api/films/' + movieIdentifier;
+const movieId = process.argv[2];
+const movieURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(movieURL, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status Code:', response.statusCode);
   } else {
-    const charactersURLs = JSON.parse(body).characters;
+    const film = JSON.parse(body);
+    const characterURLs = film.characters;
 
-    charactersURLs.forEach(characterURL => {
-      request(characterURL, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    fetchCharacters(characterURLs);
   }
 });
+
+function fetchCharacters(characterURLs) {
+  characterURLs.forEach(characterURL => {
+    request(characterURL, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+      } else if (response.statusCode !== 200) {
+        console.error('Status Code:', response.statusCode);
+      } else {
+        const character = JSON.parse(body);
+        console.log(character.name);
+      }
+    });
+  });
+}
