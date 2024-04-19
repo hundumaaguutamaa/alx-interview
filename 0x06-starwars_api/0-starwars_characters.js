32e218d795b1eccc8009eb69dd6e5b2c2a96cc54
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-19 05:25:44 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,23 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const movieURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const movieIdentifier = process.argv[2];
+const movieURL = 'https://swapi-api.alx-tools.com/api/films/' + movieIdentifier;
 
 request(movieURL, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status Code:', response.statusCode);
+    console.log(error);
   } else {
-    const film = JSON.parse(body);
-    const characterURLs = film.characters;
+    const charactersURLs = JSON.parse(body).characters;
 
-    fetchCharacters(characterURLs);
+    charactersURLs.forEach(characterURL => {
+      request(characterURL, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
   }
 });
-
-function fetchCharacters(characterURLs) {
-  characterURLs.forEach(characterURL => {
-    request(characterURL, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-      } else if (response.statusCode !== 200) {
-        console.error('Status Code:', response.statusCode);
-      } else {
-        const character = JSON.parse(body);
-        console.log(character.name);
-      }
-    });
-  });
-}
