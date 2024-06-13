fba01596c9ae1c8bcb65584c8c2ba489ebfd8d16
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-13 23:22:52 +0300

    Updated riables

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,25 +6,24 @@
 """
 
 
-def island_perimeter(matrix):
-    if not matrix or not matrix[0]:
+def island_perimeter(grid):
+    if not grid or not grid[0]:
         return 0
-
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-    total_perimeter = 0
-
-    for row in range(num_rows):
-        for col in range(num_cols):
-            if matrix[row][col] == 1:
-                if row == 0 or matrix[row-1][col] == 0:
-                    total_perimeter += 1
-                if row == num_rows-1 or matrix[row+1][col] == 0:
-                    total_perimeter += 1
-                if col == 0 or matrix[row][col-1] == 0:
-                    total_perimeter += 1
-                if col == num_cols-1 or matrix[row][col+1] == 0:
-                    total_perimeter += 1
-
-    return total_perimeter
-
+    
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+    
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                if r == 0 or grid[r-1][c] == 0:
+                    perimeter += 1
+                if r == rows-1 or grid[r+1][c] == 0:
+                    perimeter += 1
+                if c == 0 or grid[r][c-1] == 0:
+                    perimeter += 1
+                if c == cols-1 or grid[r][c+1] == 0:
+                    perimeter += 1
+    
+    return perimeter
