99c6046637169bc78a00f720c3e4ed901ef08bb8
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-07-25 15:56:17 +0300

    Implementation updated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-"""utf validation
 """
+Write a method that determines if a given data set
+represents a valid UTF-8 encoding.
+Prototype: def validUTF8(data)
+Return: True if data is a valid UTF-8 encoding, else
+return False
+"""
+
+
 def validUTF8(data):
-    """Determine if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data (list of int): List of integers representing bytes.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
+    """Determines if a given data set represents a valid UTF-8 encoding.
     """
-    
+    continuation_bytes_needed = 0
 
-    num_bytes = 0
+    UTF8_BIT_1 = 1 << 7  # 10000000
+    UTF8_BIT_2 = 1 << 6  # 01000000
 
     for byte in data:
-        if num_bytes == 0:
-            """Determine the number of bytes for this character
-            """ 
-            if (byte & 0x80) == 0:
-                num_bytes = 0
-            elif (byte & 0xE0) == 0xC0:
-                num_bytes = 1
-            elif (byte & 0xF0) == 0xE0:
-                num_bytes = 2
-            elif (byte & 0xF8) == 0xF0:
-                num_bytes = 3
-            else:
+        # Initialize a mask, check for leading 1's in the current byte.
+        leading_one_mask = 1 << 7
+
+        if continuation_bytes_needed == 0:
+            while leading_one_mask & byte:
+                continuation_bytes_needed += 1
+                leading_one_mask = leading_one_mask >> 1
+
+            if continuation_bytes_needed == 0:
+                continue
+
+            if continuation_bytes_needed == 1 or continuation_bytes_needed > 4:
                 return False
 
-            num_bytes -= 1
-    
-    return num_bytes == 0
+        else:
+            if not (byte & UTF8_BIT_1 and not (byte & UTF8_BIT_2)):
+                return False
+            
+        continuation_bytes_needed -= 1
+
+    return continuation_bytes_needed == 0
