59473e1f60faa2a5ac03939d826027910c796561
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-07-30 02:23:08 +0000

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  Pascal's Triangle  """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  
+
+    for i in range(1, n):
+        row = [1]  
+        last_row = triangle[-1]
+
+        """ Compute values in the middle of the row"""
+        for j in range(1, len(last_row)):
+            row.append(last_row[j - 1] + last_row[j])
+
+        row.append(1) 
+        triangle.append(row)
+
+    return triangle
+
