17f52e9e3ec1b5c0124979bee73db557119dd500
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-07-30 08:32:52 +0300

    Updating computing

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""  Pascal's Triangle  """
+"""Pascal's Triangle"""
 
 
 def pascal_triangle(n):
-  """ Returns a list of lists of integers
-        represens lis of integers. 
-
-  """
+    """Returns a list of lists of integers
+    representing the Pascal’s triangle of n:
+    """
 
     if n <= 0:
         return []
 
-    triangle = [[1]]  
+    """ Initialize the triangle with the first row. """
+    triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]  
+        row = [1]   
         last_row = triangle[-1]
 
-        """ Compute values in the middle of the row"""
+        """ Compute the values in the middle of the row """
         for j in range(1, len(last_row)):
             row.append(last_row[j - 1] + last_row[j])
 
-        row.append(1) 
+        row.append(1)   
         triangle.append(row)
 
     return triangle
-
+  
