4f203079a73fc1c3ebcc2cb648f6343a7253c41d
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-08 13:47:40 +0000

    Star Wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching movie data:', error);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+
+  characterUrls.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching character data:', error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
+
