ea90bfe37f952e236064829aa8bb75a1bb409f39
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-08 17:00:20 +0300

    Updating sequence

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+function fetchCharacterNames(characterUrls, index) {
+  if (index >= characterUrls.length) {
+    return;
+  }
+
+  request(characterUrls[index], (error, response, body) => {
+    if (error) {
+      console.error('Error fetching character data:', error);
+    } else {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      fetchCharacterNames(characterUrls, index + 1);
+    }
+  });
+}
 
 request(apiUrl, (error, response, body) => {
   if (error) {
@@ -13,16 +30,5 @@ request(apiUrl, (error, response, body) => {
   const movieData = JSON.parse(body);
   const characterUrls = movieData.characters;
 
-  characterUrls.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching character data:', error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+  fetchCharacterNames(characterUrls, 0);
 });
-
