1f4d35fd0969e9a8f790af850957fe045d75f3fc
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-15 11:02:22 +0300

    Pycodestyle Compliance updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,12 +12,11 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
     
-    """ Transpose the matrix """
+    # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     
-    """ Reverse each row """
+    # Reverse each row
     for i in range(n):
         matrix[i].reverse()
-      
